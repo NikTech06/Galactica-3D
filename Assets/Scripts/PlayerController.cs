eef36903736e5c20fc9255a71acd033a1925f047
Nlk29 <78368286+NikTech06@users.noreply.github.com>
2022-12-05 20:52:32 +0100

    2052051222

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,83 +5,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
-    public Transform  camera;
+    public Transform camera;
 
-    
-    [Header("REFERENCES")]
-    [SerializeField] private Rigidbody _rb;
-    [SerializeField] private Target _target;
-    [SerializeField] private GameObject _explosionPrefab;
-
-    [Header("MOVEMENT")]
     [Range(0, 30)]
-    public float accelerateSpeed = 10f;
-    [SerializeField] private float _rotateSpeed = 95;
+    [SerializeField] private float accelerateSpeed = 10f;
+    [SerializeField] private float rotateSpeed = 3f;
 
-    [Header("PREDICTION")]
-    [SerializeField] private float _maxDistancePredict = 100;
-    [SerializeField] private float _minDistancePredict = 5;
-    [SerializeField] private float _maxTimePrediction = 5;
-    private Vector3 _standardPrediction, _deviatedPrediction;
-
-    [Header("DEVIATION")]
-    [SerializeField] private float _deviationAmount = 50;
-    [SerializeField] private float _deviationSpeed = 2;
-
-    public float defaultAngularDrag = 0.05f;
-    public float gravityZoneAngularDrag = 10f;
-
-    
+    Quaternion rot;
 
     void Update()
     {
-        Accelerate();
-
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        rb.useGravity = true;
-        rb.angularDrag = gravityZoneAngularDrag;
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        rb.useGravity = false;
-        rb.angularDrag = defaultAngularDrag;
-    }
-
-    void Accelerate()
-    {
+        //moving
         if (Input.GetButton("Accelerate"))
         {
             rb.AddForce(Vector3.back * accelerateSpeed * Time.deltaTime * 100);
         }
-    }
 
-    private void FixedUpdate()
-    {
-        var leadTimePercentage = Mathf.InverseLerp(_minDistancePredict, _maxDistancePredict, Vector3.Distance(transform.position, _target.transform.position));
+        if (Input.GetButton("Brake"))
+        {
+            rb.AddForce(Vector3.forward * accelerateSpeed * Time.deltaTime * 100);
+        }
 
-        PredictMovement(leadTimePercentage);
-
-        RotateRocket();
-    }
-
-    private void PredictMovement(float leadTimePercentage)
-    {
-        var predictionTime = Mathf.Lerp(0, _maxTimePrediction, leadTimePercentage);
-
-        _standardPrediction = _target.Rb.position + _target.Rb.velocity * predictionTime;
-    }
-
-    private void RotateRocket()
-    {
-        var heading = _deviatedPrediction - transform.position;
-
-
-        var rotation = Quaternion.LookRotation(heading);
-        _rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, rotation, _rotateSpeed * Time.deltaTime));
+        //rotating
+        Vector3 rotationAmount = camera.rotation - transform.rotation;
+        rot.eulerAngles = rotationAmount;
+        transform.rotation = rot;
     }
 }
 
