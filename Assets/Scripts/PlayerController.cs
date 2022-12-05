9bc34b7439c36a92a427883672d87cb73bff21cc
Nlk29 <78368286+NikTech06@users.noreply.github.com>
2022-12-05 16:30:48 +0100

    1630051222

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
+    public Transform  camera;
 
+    
+    [Header("REFERENCES")]
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private Target _target;
+    [SerializeField] private GameObject _explosionPrefab;
+
+    [Header("MOVEMENT")]
     [Range(0, 30)]
-    public float speed = 10f;
+    public float accelerateSpeed = 10f;
+    [SerializeField] private float _rotateSpeed = 95;
+
+    [Header("PREDICTION")]
+    [SerializeField] private float _maxDistancePredict = 100;
+    [SerializeField] private float _minDistancePredict = 5;
+    [SerializeField] private float _maxTimePrediction = 5;
+    private Vector3 _standardPrediction, _deviatedPrediction;
+
+    [Header("DEVIATION")]
+    [SerializeField] private float _deviationAmount = 50;
+    [SerializeField] private float _deviationSpeed = 2;
 
     public float defaultAngularDrag = 0.05f;
     public float gravityZoneAngularDrag = 10f;
 
+    
+
     void Update()
     {
-        if (Input.GetButton("Accelerate"))
-        {
-            rb.AddForce(Vector3.back * speed * Time.deltaTime * 100);
-        }
+        Accelerate();
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -31,4 +50,38 @@ public class PlayerController : MonoBehaviour
         rb.useGravity = false;
         rb.angularDrag = defaultAngularDrag;
     }
+
+    void Accelerate()
+    {
+        if (Input.GetButton("Accelerate"))
+        {
+            rb.AddForce(Vector3.back * accelerateSpeed * Time.deltaTime * 100);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        var leadTimePercentage = Mathf.InverseLerp(_minDistancePredict, _maxDistancePredict, Vector3.Distance(transform.position, _target.transform.position));
+
+        PredictMovement(leadTimePercentage);
+
+        RotateRocket();
+    }
+
+    private void PredictMovement(float leadTimePercentage)
+    {
+        var predictionTime = Mathf.Lerp(0, _maxTimePrediction, leadTimePercentage);
+
+        _standardPrediction = _target.Rb.position + _target.Rb.velocity * predictionTime;
+    }
+
+    private void RotateRocket()
+    {
+        var heading = _deviatedPrediction - transform.position;
+
+
+        var rotation = Quaternion.LookRotation(heading);
+        _rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, rotation, _rotateSpeed * Time.deltaTime));
+    }
 }
+
